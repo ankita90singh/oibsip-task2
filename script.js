4c3963a5aeb4f79c57244d5d759e7815ab7edbb5
ankita90singh <138467717+ankita90singh@users.noreply.github.com>
2023-08-04 21:54:23 +0530

    task2

---
+++ script.js
@@ -0,0 +1,264 @@
+gsap.from("#img1",{
+    x:10,
+    opacity:0,
+    duration:1,
+    scale:0,
+    delay:.5,
+    scrollTrigger: {
+        trigger: "#img1",
+        scroller: "body",
+        // markers:true,
+        start: "top 40%",
+        end: "top 45%",
+        scrub: 1,
+    }
+})
+gsap.from("#img2",{
+    x:-10,
+    opacity:0,
+    duration:1,
+    scale:0,
+    delay:.9,
+    scrollTrigger: {
+        trigger: "#img2",
+        scroller: "body",
+        // markers:true,
+        start: "top 60%",
+        end: "top 75%",
+        scrub: 1,
+    }
+})
+
+gsap.from("#page2 h1 , #page2 h4",{
+    x:-100,
+    opacity:0,
+    duration:1,
+    delay:.5,
+    stagger:0.5,
+    scrollTrigger: {
+        trigger: "#page2",
+        scroller: "body",
+        // markers:true,
+        start: "top 40%",
+        end: "top 45%",
+        scrub: 1,
+    }
+})
+
+gsap.from(".page2r p",{
+    x:100,
+    opacity:0,
+    duration:1,
+    delay:.5,
+    // stagger:0.5,
+    scrollTrigger: {
+        trigger: ".page2r",
+        scroller: "body",
+        // markers:true,
+        start: "top 50%",
+        end: "top 55%",
+        scrub: 1,
+    }
+})
+
+gsap.from("#img3",{
+    x:-10,
+    opacity:0,
+    duration:1,
+    scale:0,
+    delay:.5,
+    stagger:0.5,
+    scrollTrigger: {
+        trigger: "#img3",
+        scroller: "body",
+        // markers:true,
+        start: "top 30%",
+        end: "top 45%",
+        scrub: 1,
+    }
+})
+
+
+
+gsap.from("#page3 h1",{
+    x:-100,
+    opacity:0,
+    duration:1,
+    delay:.5,
+    stagger:0.5,
+    scrollTrigger: {
+        trigger: "#page3",
+        scroller: "body",
+        // markers:true,
+        start: "top 30%",
+        end: "top 45%",
+        scrub: 1,
+    }
+})
+
+gsap.from("#img4",{
+    x:10,
+    opacity:0,
+    duration:1,
+    scale:0,
+    delay:.5,
+    stagger:0.5,
+    scrollTrigger: {
+        trigger: "#img4",
+        scroller: "body",
+        // markers:true,
+        start: "top 35%",
+        end: "top 45%",
+        scrub: 1,
+    }
+})
+
+gsap.from("#img5",{
+    x:-10,
+    opacity:0,
+    duration:1,
+    delay:.5,
+    scale:0,
+    stagger:0.5,
+    scrollTrigger: {
+        trigger: "#img5",
+        scroller: "body",
+        // markers:true,
+        start: "top 30%",
+        end: "top 45%",
+        scrub: 1,
+    }
+})
+
+
+
+gsap.from("#page4 h1",{
+    x:-100,
+    opacity:0,
+    duration:1,
+    delay:.5,
+    stagger:0.5,
+    scrollTrigger: {
+        trigger: "#page4",
+        scroller: "body",
+        // markers:true,
+        start: "top 30%",
+        end: "top 45%",
+        scrub: 1,
+    }
+})
+
+gsap.from("#img6",{
+    x:10,
+    opacity:0,
+    duration:1,
+    scale:0,
+    delay:.5,
+    stagger:0.5,
+    scrollTrigger: {
+        trigger: "#img6",
+        scroller: "body",
+        // markers:true,
+        start: "top 75%",
+        end: "top 85%",
+        scrub: 1,
+    }
+})
+
+
+gsap.from("#page5 h1",{
+    x:-100,
+    opacity:0,
+    duration:1,
+    delay:.5,
+    stagger:0.5,
+    scrollTrigger: {
+        trigger: "#page5",
+        scroller: "body",
+        // markers:true,
+        start: "top 30%",
+        end: "top 45%",
+        scrub: 1,
+    }
+})
+
+
+gsap.from("#txtp, #skills,video",{
+    x:5,
+    scale:0,
+    opacity:0,
+    duration:1,
+    delay:.5,
+    stagger:0.3,
+    scrollTrigger: {
+        trigger: "#txtp",
+        scroller: "body",
+        // markers:true,
+        start: "top 65%",
+        end: "top 75%",
+        scrub: 1,
+    }
+})
+gsap.from("#page6 h1",{
+    x:-100,
+    opacity:0,
+    duration:1,
+    delay:.5,
+    stagger:0.5,
+    scrollTrigger: {
+        trigger: "#page6",
+        scroller: "body",
+        // markers:true,
+        start: "top 30%",
+        end: "top 45%",
+        scrub: 1,
+    }
+})
+
+gsap.from("#img8",{
+    x:-100,
+    opacity:0,
+    duration:1,
+    delay:.5,
+    stagger:0.5,
+    scrollTrigger: {
+        trigger: "#img8",
+        scroller: "body",
+        // markers:true,
+        start: "top 30%",
+        end: "top 45%",
+        scrub: 1,
+    }
+})
+gsap.from("#page7 h1",{
+    x:100,
+    opacity:0,
+    duration:1,
+    delay:.5,
+    stagger:0.5,
+    scrollTrigger: {
+        trigger: "#page7",
+        scroller: "body",
+        // markers:true,
+        start: "top 30%",
+        end: "top 45%",
+        scrub: 1,
+    }
+})
+
+gsap.from("#page8 h1,#page8 p",{
+    x:100,
+    opacity:0,
+    duration:1,
+    scale:0,
+    delay:.5,
+    stagger:0.3,
+    scrollTrigger: {
+        trigger: "#page8",
+        scroller: "body",
+        // markers:true,
+        start: "top 30%",
+        end: "top 45%",
+        scrub: 3,
+    }
+})
